f628e0c9603d82053dc7a2db236d3284d57d0c5f
ostap <ostap.ja@gmail.com>
2025-11-16 14:58:00 +0200

    register simplification

--- src/main/java/com/komplikevych/AccountManagement/dto/request/RegistrationRequest.java
+++ src/main/java/com/komplikevych/AccountManagement/dto/request/RegistrationRequest.java
@@ -11,10 +11,8 @@ import java.time.LocalDate;
 
 @Builder
 public record RegistrationRequest(
-        @NotBlank(message = "First name is required")
         @Size(max = 100, message = "First name must not exceed 100 characters")
         String firstName,
-        @NotBlank(message = "Last name is required")
         @Size(max = 100, message = "Last name must not exceed 100 characters")
         String lastName,
         Gender gender,
--- src/main/java/com/komplikevych/AccountManagement/model/entity/User.java
+++ src/main/java/com/komplikevych/AccountManagement/model/entity/User.java
@@ -32,10 +32,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "first_name", nullable = false, length = 100)
+    @Column(name = "first_name", length = 100)
     private String firstName;
 
-    @Column(name = "last_name", nullable = false, length = 100)
+    @Column(name = "last_name", length = 100)
     private String lastName;
 
     @Column(name = "middle_name", length = 100)
